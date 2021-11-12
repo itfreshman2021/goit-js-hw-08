a33f13c298cd2423b326064a32b1d497c788ed10
Ruslan Filonchuk <itfreshman@ukr.net>
2021-11-12 20:09:21 +0200

    Додано рішення завдання №1
    Додано рішення завдання №1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,56 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+        return  `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+`;
+})
+.join("");
+
+}
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+    const isGalleryImageEl = evt.target.classList.contains("gallery__image");
+
+    if (!isGalleryImageEl) {
+        return;
+    }
+
+    const urlBigImageEl = evt.target.getAttribute("data-source");
+  const gallery = new SimpleLightbox('.gallery a', {
+    widthRatio: 0.95,
+    heightRatio: 1.00,
+    captionsData: "alt",
+    captionDelay: 250,
+    });
+    gallery.on('show.simplelightbox', function () {
+       
+
+});
+    
+    gallery.on('error.simplelightbox', function (e) {
+    
+	console.log(e); 
+});   
+}
