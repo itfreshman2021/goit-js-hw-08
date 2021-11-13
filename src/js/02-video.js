0dfa000314c7dab95e8525133ff4e75afd6eb042
Ruslan Filonchuk <itfreshman@ukr.net>
2021-11-13 19:24:56 +0200

    Додано рішення завдання №2
    Додано рішення завдання №2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+
+const savedVideoplayerCurrentTime = localStorage.getItem("videoplayer-current-time");
+const VideoPlayerStartTime  = JSON.parse(savedVideoplayerCurrentTime);
+
+
+player.setCurrentTime(VideoPlayerStartTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+player.on('timeupdate', throttle(onPlayerTimeupdate, 1000));
+
+function onPlayerTimeupdate(data) {
+    const videoplayerCurrentTime = data.seconds;
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(videoplayerCurrentTime));
+    
+}
+
