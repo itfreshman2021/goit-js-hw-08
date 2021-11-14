590f55be8c532d11885bb2c5ff9ab8225a1fa7e0
Ruslan Filonchuk <itfreshman@ukr.net>
2021-11-14 17:38:36 +0200

    Додано рішення завдання №3
    Додано рішення завдання №3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = "feedback-form-state";
+const message = {};
+
+const refsForm = document.querySelector(".feedback-form");
+
+refsForm.addEventListener("input", throttle(onFormInput, 500));
+refsForm.addEventListener("submit", onFormSubmit);
+
+populateForm();
+
+function onFormInput(event) {
+
+    const { name, value } = event.target;
+    message[name] = value;
+    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(message));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.log(message);
+
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function populateForm () {
+    const savedDataForm = localStorage.getItem(STORAGE_KEY);
+    const parsedDataForm = JSON.parse(savedDataForm);
+
+    if (parsedDataForm) {
+        
+        refsForm.elements.email.value = parsedDataForm.email;
+        refsForm.elements.message.value = parsedDataForm.message;
+    }
+};
